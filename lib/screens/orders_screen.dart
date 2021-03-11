7f7bbf571cd4dc6815f536cc79b8675eae9d2771
samirbhatt97 <53816866+samirbhatt97@users.noreply.github.com>
2021-03-11 17:30:45 +0300

    Commit Lecture 258*
    258. Using the "FutureBuilder" Widget &
    Improving the Code

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -12,40 +12,79 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
+    _ordersFuture = _obtainOrdersFuture();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    //final orderData = Provider.of<Orders>(context);
+    // commented above as it would make build method recalled each time due to..
+    //.. the FutureBuilder down below, hence cause the page to be in a constant loop
+    print('building Orders');
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        //future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        future: _ordersFuture,
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, i) => OrderItem(
-                orderData.orders[i],
-              ),
-            ),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              //..
+              // do Error Handling stuff
+              return Center(
+                child: Text('An error occured!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(
+                    orderData.orders[i],
+                  ),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
+
+//var _isLoading = false;
+// @override
+// void initState() {
+//   _isLoading = true;
+//   Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+//     setState(() {
+//       _isLoading = false;
+//     });
+//   });
+//   //--------------------- OR ----------------------//
+//   //Future.delayed(Duration.zero).then((_) async {
+//   // setState(() {
+//   //   _isLoading = true;
+//   // });
+//   //await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+//   //setState(() {
+//   //  _isLoading = false;
+//   //});
+//   //});
+//   super.initState();
+// }
